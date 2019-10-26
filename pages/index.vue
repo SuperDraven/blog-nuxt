35d866a8b3ed3596b57ddd99d855ddb785fcf112
lld <375845787@qq.com>
2019-10-26 12:31:12 +0800

    测试 自动部署

--- pages/index.vue
+++ pages/index.vue
@@ -22,7 +22,7 @@
                     <Icon type="md-eye" /> {{ item.pv }}
                   </li>
                   <li>
-                    <Icon type="ios-thumbs-up-outline" /> 2354
+                    <Icon type="ios-thumbs-up-outline" /> 23
                   </li>
                   <li>
                     <Icon type="ios-chatbubbles-outline" /> 345
